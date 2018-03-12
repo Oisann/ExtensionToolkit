f8f7d85b4ca3695c2cdaac0d44a6a30d218b0f31
Trainee <trainee@torbjrns-imac.local>
2018-03-12 11:46:43 +0100

    Initial Commit

---
+++ ExtensionToolkit/ExtensionToolkit.h
@@ -0,0 +1,19 @@
+//
+//  ExtensionToolkit.h
+//  ExtensionToolkit
+//
+//  Created by Trainee on 12/03/2018.
+//  Copyright © 2018 Trainee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExtensionToolkit.
+FOUNDATION_EXPORT double ExtensionToolkitVersionNumber;
+
+//! Project version string for ExtensionToolkit.
+FOUNDATION_EXPORT const unsigned char ExtensionToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExtensionToolkit/PublicHeader.h>
+
+

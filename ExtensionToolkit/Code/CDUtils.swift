84fea877c61a0cc798caff68fad534835aeb3e35
Trainee <trainee@Torbjrns-iMac.local>
2018-03-23 14:04:58 +0100

    Added more extensions and fixed some bugs in the downloads

--- ExtensionToolkit/Code/CDUtils.swift
+++ ExtensionToolkit/Code/CDUtils.swift
@@ -10,29 +10,45 @@ import CoreData
 
 public class CDUtils {
     
-    public static func saveDataDictionary(_ dictionary: [[String: Any]], entity: String, context: NSManagedObjectContext, completionHandler: () -> Void) {
-        for dict in dictionary as [[String: Any]] {
-            CDUtils.saveDataObject(entityName: entity, dictionary: dict, parent: nil, context: context)
+    public static var managedContext: NSManagedObjectContext!
+    
+    public static func fetch<T:NSManagedObject>(resultType: T.Type, filter: NSPredicate? = nil) -> [T]? {
+        let fetchRequest = NSFetchRequest<T>(entityName: String(describing: T.self))
+        fetchRequest.predicate = filter
+        
+        do {
+            return try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+            return nil
         }
-        saveContext(context: context)
+    }
+    
+    public static func saveDataDictionary(_ dictionary: [[String: Any]], entity: String, completionHandler: () -> Void) {
+        for dict in dictionary as [[String: Any]] {
+            CDUtils.saveDataObject(entityName: entity, dictionary: dict, parent: nil)
+        }
+        saveContext()
         completionHandler()
     }
     
-    public static func saveContext(context: NSManagedObjectContext) {
-        if context.hasChanges {
+    public static func saveContext() {
+        if managedContext.hasChanges {
             do {
-                try context.save()
+                try managedContext.save()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
+        } else {
+            print("No changes to save")
         }
     }
     
-    public static func saveDataObject(entityName: String, dictionary: [String: Any], parent: (entityName: String, value: NSManagedObject)?, context: NSManagedObjectContext) {
-        let dataObject = NSEntityDescription.insertNewObject(forEntityName: entityName, into: context)
+    public static func saveDataObject(entityName: String, dictionary: [String: Any], parent: (entityName: String, value: NSManagedObject)?) {
+        let dataObject = NSEntityDescription.insertNewObject(forEntityName: entityName, into: managedContext)
         
         if let parent = parent {
             dataObject.setValue(parent.value, forKey: parent.entityName)
@@ -45,7 +61,7 @@ public class CDUtils {
                         continue
                     }
                     for relationDictionary in values {
-                        saveDataObject(entityName: destinationName, dictionary: relationDictionary, parent: (entityName, dataObject), context: context)
+                        saveDataObject(entityName: destinationName, dictionary: relationDictionary, parent: (entityName, dataObject))
                     }
                 } else if let attributeDescription = property as? NSAttributeDescription {
                     if attributeDescription.attributeType == .dateAttributeType, let dateString = kvp.value as? String {
--- ExtensionToolkit/Code/Extensions.swift
+++ ExtensionToolkit/Code/Extensions.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 public extension Int {
     
@@ -39,5 +40,63 @@ public extension CGFloat {
      */
     public static func parseHMS(hms: String) -> CGFloat {
         return CGFloat(Int.parseHMS(hms: hms))
+    }
+}
+
+public enum Fill {
+    case dotted
+    case line
+    case space
+}
+
+public extension UILabel {
+    
+    public func fillRemaining(_ fill: Fill, offset: CGFloat = 16) {
+        
+        guard let attributedText = self.attributedText else {
+            var string = NSString(string: self.text!)
+            var textSize: CGSize = string.size(withAttributes: [NSAttributedStringKey.font: self.font])
+            
+            while (textSize.width < self.bounds.width - offset) {
+                self.text = self.text! + stringForFill(fill)
+                
+                string = NSString(string: self.text!)
+                textSize = string.size(withAttributes: [NSAttributedStringKey.font: self.font])
+            }
+            return
+        }
+        
+        let textSize: CGSize = attributedText.size()
+        let font = UIFont(name: "AvenirNext-Regular", size: self.font.pointSize) ?? UIFont.systemFont(ofSize: self.font.pointSize)
+        let fillString: NSMutableAttributedString = NSMutableAttributedString(string: "", attributes: [NSAttributedStringKey.font: font])
+        
+        while (textSize.width + fillString.size().width < self.bounds.width - offset) {
+            let filler = NSAttributedString(string: stringForFill(fill), attributes: [NSAttributedStringKey.font: font])
+            fillString.append(filler)
+        }
+        self.attributedText! = NSMutableAttributedString(attributedString: self.attributedText!).appending(fillString)
+    }
+    
+    fileprivate func stringForFill(_ fill: Fill) -> String {
+        
+        if fill == .dotted {
+            return "."
+        }
+        
+        if fill == .line {
+            return "_"
+        }
+        
+        return " "
+    }
+}
+
+public extension NSMutableAttributedString {
+    
+    //Appends a NSAttributedString to a new instance of the NSMutableAttributedString and returns the new instance
+    public func appending(_ string: NSAttributedString) -> NSMutableAttributedString {
+        let l = NSMutableAttributedString(attributedString: self)
+        l.append(string)
+        return l
     }
 }
